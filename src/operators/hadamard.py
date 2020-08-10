a7a4bc727286200585da2cf8c7b4ed2f2dbe94e1
Bruno Furtado <brunofurmon@gmail.com>
2020-08-09 22:08:13 -0300

    Better doc

--- src/operators/hadamard.py
+++ src/operators/hadamard.py
@@ -4,7 +4,7 @@ from numpy import array, tensordot, sqrt
 def H(input):
     ''' Hadamard operation on 1 qbit\n
     @input qbit must be in row form, like [[0],[1]]\n
-    returns the superposition state of this qbit
+    returns the superposition state of @input qbit
     '''
     hadamardMatrix = (1 / sqrt(2)) * array([[1, 1],
                                             [1, -1]])
